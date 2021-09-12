89c31f2499a5eda06f03b6f9d78865d83aa18424
douguizilla <douglasgommez@gmail.com>
2021-09-12 18:10:19 -0300

    implemented BookDetailsActivity.kt

--- app/src/main/java/com/odougle/books/BookDetailsActivity.kt
+++ app/src/main/java/com/odougle/books/BookDetailsActivity.kt
@@ -1,8 +1,12 @@
 package com.odougle.books
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.odougle.books.databinding.ActivityBookDetailsBinding
+import com.odougle.books.model.Book
+import org.parceler.Parcels
 
 class BookDetailsActivity : AppCompatActivity() {
 
@@ -11,6 +15,24 @@ class BookDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityBookDetailsBinding.inflate(layoutInflater)
+
+        val book = Parcels.unwrap<Book>(intent.getParcelableExtra(EXTRA_BOOK))
+        if(book != null){
+            binding.book = book
+        }
+
         setContentView(binding.root)
+    }
+
+    companion object{
+        private const val EXTRA_BOOK = "book"
+
+        fun start(context: Context, book: Book){
+            context.startActivity(
+                Intent(context, BookDetailsActivity::class.java).apply {
+                    putExtra(EXTRA_BOOK, Parcels.wrap(book))
+                }
+            )
+        }
     }
 }
