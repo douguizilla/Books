8de7683715ebbd59cfeea72aa775db7a9f861fcd
douguizilla <douglasgommez@gmail.com>
2021-09-12 16:41:42 -0300

    Initial commit

---
+++ app/src/main/java/com/odougle/books/BookListActivity.kt
@@ -0,0 +1,11 @@
+package com.odougle.books
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BookListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
