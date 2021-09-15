3ea1ecef8b4cbbc37b4bbabee47b5cc5b0f2602d
douguizilla <douglasgommez@gmail.com>
2021-09-14 21:26:55 -0300

    modified BookListActivity.kt to see the result

--- app/src/main/java/com/odougle/books/BookListActivity.kt
+++ app/src/main/java/com/odougle/books/BookListActivity.kt
@@ -3,6 +3,9 @@ package com.odougle.books
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.odougle.books.databinding.ActivityBookListBinding
+import com.odougle.books.model.Book
+import com.odougle.books.model.MediaType
+import com.odougle.books.model.Publisher
 
 class BookListActivity : AppCompatActivity() {
     private lateinit var binding : ActivityBookListBinding
@@ -11,6 +14,20 @@ class BookListActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityBookListBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        BookDetailsActivity.start(this,
+        Book().apply {
+            id = "1"
+            title = "Dominando o Android com Kotlin"
+            author = "Nelson Glauber"
+            coverUrl = "https://s3.novatec.com.br/capas-ampliadas/capa-ampliada-9788575224632.jpg"
+            pages = 954
+            year = 2018
+            publisher = Publisher("1","Novatec")
+            available = true
+            mediaType = MediaType.EBOOK
+            rating = 5.0f
+        }
+        )
     }
 
 
