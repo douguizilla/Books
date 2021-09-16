d2cac98cac70a9ca7264fdf7a6278743ed789ede
douguizilla <douglasgommez@gmail.com>
2021-09-16 14:25:37 -0300

    implemented BookListActivity.kt

--- app/src/main/java/com/odougle/books/BookListActivity.kt
+++ app/src/main/java/com/odougle/books/BookListActivity.kt
@@ -2,32 +2,39 @@ package com.odougle.books
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.odougle.books.adapter.BookAdapter
 import com.odougle.books.databinding.ActivityBookListBinding
 import com.odougle.books.model.Book
 import com.odougle.books.model.MediaType
 import com.odougle.books.model.Publisher
 
 class BookListActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityBookListBinding
+    private lateinit var binding: ActivityBookListBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityBookListBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        BookDetailsActivity.start(this,
-        Book().apply {
-            id = "1"
-            title = "Dominando o Android com Kotlin"
-            author = "Nelson Glauber"
-            coverUrl = "https://s3.novatec.com.br/capas-ampliadas/capa-ampliada-9788575224632.jpg"
-            pages = 954
-            year = 2018
-            publisher = Publisher("1","Novatec")
-            available = true
-            mediaType = MediaType.EBOOK
-            rating = 5.0f
-        }
+        val books = listOf(
+            Book().apply {
+                id = "1"
+                title = "Dominando o Android com Kotlin"
+                author = "Nelson Glauber"
+                coverUrl =
+                    "https://s3.novatec.com.br/capas-ampliadas/capa-ampliada-9788575224632.jpg"
+                pages = 954
+                year = 2018
+                publisher = Publisher("1", "Novatec")
+                available = true
+                mediaType = MediaType.EBOOK
+                rating = 5.0f
+            }
         )
+        binding.rvBooks.layoutManager = LinearLayoutManager(this)
+        binding.rvBooks.adapter = BookAdapter(books) { book ->
+            BookDetailsActivity.start(this, book)
+        }
     }
 
 
