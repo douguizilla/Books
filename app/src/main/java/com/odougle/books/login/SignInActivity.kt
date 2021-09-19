980528c6f3647aafd4b48ca17218eb051141458e
douguizilla <douglasgommez@gmail.com>
2021-09-19 14:53:20 -0300

    fix dependencies

--- app/src/main/java/com/odougle/books/login/SignInActivity.kt
+++ app/src/main/java/com/odougle/books/login/SignInActivity.kt
@@ -3,10 +3,18 @@ package com.odougle.books.login
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.firebase.auth.FirebaseAuth
+
 import com.odougle.books.R
 import com.odougle.books.databinding.ActivitySignInBinding
 
 class SignInActivity : AppCompatActivity() {
+
+    private var googleApiClient: GoogleApiClient? = null
+    private var fbAuth = FirebaseAuth.getInstance()
 
     private val binding : ActivitySignInBinding by lazy<ActivitySignInBinding> {
         DataBindingUtil.setContentView(this, R.layout.activity_sign_in)
@@ -15,5 +23,23 @@ class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        initGoogleSignIn()
+        binding.btnSignIn.setOnClickListener {
+            signIn()
+        }
+    }
+
+    private fun initGoogleSignIn(){
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleApiClient = GoogleApiClient.Builder(this)
+            .enableAutoManage(this){
+                showErrorSignIn()
+            }
+            .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+            .build()
     }
 }
